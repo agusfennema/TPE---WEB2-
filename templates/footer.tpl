9c75d9cbe53f2863805b101694c0022c58b580bb
Agus Fennema <agusfennema@gmail.com>
2023-10-15 20:00:14 -0300

    actualizado footer

--- templates/footer.tpl
+++ templates/footer.tpl
@@ -3,9 +3,9 @@
       <div>
         <div>
           <h4>Contacto</h4>
-          <p>Teléfono: +123456789</p>
-          <p>Email: info@tuempresa.com</p>
-          <p>Dirección: Calle Principal #123, Ciudad</p>
+          <p>Celular: +249010001</p>
+          <p>Email: annyvia@products.com</p>
+          <p>Dirección: Avenida Siempre Viva 123, Tandil</p>
         </div>
         <div>
           <h4>Enlaces Rápidos</h4>
@@ -21,7 +21,7 @@
           <h4>Síguenos</h4>
           <ul>
             <li><a href="#">Facebook</a></li>
-            <li><a href="#">Instagram</a></li>
+            <li><a href="https://www.instagram.com/aguss_fennema/">Instagram</a></li>
             <li><a href="#">Twitter</a></li>
             <li><a href="#">Pinterest</a></li>
           </ul>
--- templates_c/2fd0fc615e37415c2b895365ce08111eb3709aa5_0.file.footer.tpl.php
+++ templates_c/2fd0fc615e37415c2b895365ce08111eb3709aa5_0.file.footer.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.3.2, created on 2023-10-15 23:48:31
+/* Smarty version 4.3.2, created on 2023-10-16 00:59:00
   from 'C:\xampp\htdocs\TPE---WEB2-\templates\footer.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.2',
-  'unifunc' => 'content_652c5e2f727de9_93029695',
+  'unifunc' => 'content_652c6eb41a6941_95375529',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '2fd0fc615e37415c2b895365ce08111eb3709aa5' => 
     array (
       0 => 'C:\\xampp\\htdocs\\TPE---WEB2-\\templates\\footer.tpl',
-      1 => 1696428234,
+      1 => 1697410737,
       2 => 'file',
     ),
   ),
@@ -20,15 +20,15 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_652c5e2f727de9_93029695 (Smarty_Internal_Template $_smarty_tpl) {
+function content_652c6eb41a6941_95375529 (Smarty_Internal_Template $_smarty_tpl) {
 ?><footer class="footer">
     <div class="container">
       <div>
         <div>
           <h4>Contacto</h4>
-          <p>Teléfono: +123456789</p>
-          <p>Email: info@tuempresa.com</p>
-          <p>Dirección: Calle Principal #123, Ciudad</p>
+          <p>Celular: +249010001</p>
+          <p>Email: annyvia@products.com</p>
+          <p>Dirección: Avenida Siempre Viva 123, Tandil</p>
         </div>
         <div>
           <h4>Enlaces Rápidos</h4>
@@ -44,7 +44,7 @@ function content_652c5e2f727de9_93029695 (Smarty_Internal_Template $_smarty_tpl)
           <h4>Síguenos</h4>
           <ul>
             <li><a href="#">Facebook</a></li>
-            <li><a href="#">Instagram</a></li>
+            <li><a href="https://www.instagram.com/aguss_fennema/">Instagram</a></li>
             <li><a href="#">Twitter</a></li>
             <li><a href="#">Pinterest</a></li>
           </ul>
