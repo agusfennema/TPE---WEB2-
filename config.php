ed0874da5c10582526dd25544e6aa0498f464da5
Agus Fennema <agusfennema@gmail.com>
2023-10-17 12:28:55 -0300

    arreglado archivo config

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
     // Configuración de la conexión a la base de datos
-    define('HOST', 'localhost'); // Dirección del servidor de la base de datos
-    define('DBNAME', 'tienda'); // Nombre de la base de datos
-    define('USERNAME', 'webadmin');// Usuario de la base de datos
-    define('PASSWORD', '$2y$10$JC8JHq4ObWBWmvsF7IyM/OfZ51H0LKQ3epRwCw5AlQPDW3P6TSsLe');// Contraseña de la base de datos
-
+    define('DB_HOST', 'localhost'); // Dirección del servidor de la base de datos
+    define('DB_NAME', 'tienda'); // Nombre de la base de datos
+    define('DB_USERNAME', 'webadmin');// Usuario de la base de datos
+    define('DB_PASSWORD', '$2y$10$JC8JHq4ObWBWmvsF7IyM/OfZ51H0LKQ3epRwCw5AlQPDW3P6TSsLe');// Contraseña de la base de datos
+    define('DB_CHARSET', 'utf8'); // Codificación de caracteres en UTF-8.
 ?>
