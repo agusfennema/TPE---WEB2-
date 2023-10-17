2ca6beb081a5e642955c4784c68d0bf35b7b6eca
Agus Fennema <agusfennema@gmail.com>
2023-10-17 12:22:57 -0300

    creado archivo config

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+    // Configuración de la conexión a la base de datos
+    define('HOST', 'localhost'); // Dirección del servidor de la base de datos
+    define('DBNAME', 'tienda'); // Nombre de la base de datos
+    define('USERNAME', 'web@admin.com');// Usuario de la base de datos
+    define('PASSWORD', '$2y$10$JC8JHq4ObWBWmvsF7IyM/OfZ51H0LKQ3epRwCw5AlQPDW3P6TSsLe');// Contraseña de la base de datos
+
+?>
