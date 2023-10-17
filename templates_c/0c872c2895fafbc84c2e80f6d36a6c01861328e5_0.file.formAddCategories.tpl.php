648068edf3eea1323ec915eaa56c1d85f05e80bc
Agus Fennema <agusfennema@gmail.com>
2023-10-17 11:29:11 -0300

    arreglado fondo

--- templates_c/0c872c2895fafbc84c2e80f6d36a6c01861328e5_0.file.formAddCategories.tpl.php
+++ templates_c/0c872c2895fafbc84c2e80f6d36a6c01861328e5_0.file.formAddCategories.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.3.2, created on 2023-10-17 15:43:06
+/* Smarty version 4.3.2, created on 2023-10-17 16:19:36
   from 'C:\xampp\htdocs\TPE---WEB2-\templates\formAddCategories.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.2',
-  'unifunc' => 'content_652e8f6a369667_60912066',
+  'unifunc' => 'content_652e97f8ed6eb7_33556395',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '0c872c2895fafbc84c2e80f6d36a6c01861328e5' => 
     array (
       0 => 'C:\\xampp\\htdocs\\TPE---WEB2-\\templates\\formAddCategories.tpl',
-      1 => 1697550153,
+      1 => 1697550224,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_652e8f6a369667_60912066 (Smarty_Internal_Template $_smarty_tpl) {
+function content_652e97f8ed6eb7_33556395 (Smarty_Internal_Template $_smarty_tpl) {
 ?><form class="formulario" action="addCategorie" method="POST">
     <div>
         <label class="form-label">Categoria</label>
