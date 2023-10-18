e9539beb8e5891dbbc9ad81beff34e778043c34d
Agus Fennema <agusfennema@gmail.com>
2023-10-17 23:34:27 -0300

    modificacion

--- templates_c/11ba75a807a7ecc5ba0c7042a1321405ab04d7f1_0.file.formEdit.tpl.php
+++ templates_c/11ba75a807a7ecc5ba0c7042a1321405ab04d7f1_0.file.formEdit.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.3.2, created on 2023-10-17 15:44:20
+/* Smarty version 4.3.2, created on 2023-10-18 04:27:49
   from 'C:\xampp\htdocs\TPE---WEB2-\templates\formEdit.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.2',
-  'unifunc' => 'content_652e8fb46f20a5_33046036',
+  'unifunc' => 'content_652f42a529dec9_16240190',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '11ba75a807a7ecc5ba0c7042a1321405ab04d7f1' => 
     array (
       0 => 'C:\\xampp\\htdocs\\TPE---WEB2-\\templates\\formEdit.tpl',
-      1 => 1697548886,
+      1 => 1697595857,
       2 => 'file',
     ),
   ),
@@ -22,7 +22,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:footer.tpl' => 1,
   ),
 ),false)) {
-function content_652e8fb46f20a5_33046036 (Smarty_Internal_Template $_smarty_tpl) {
+function content_652f42a529dec9_16240190 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender("file:header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
 
@@ -41,7 +41,7 @@ $_smarty_tpl->_subTemplateRender("file:header.tpl", $_smarty_tpl->cache_id, $_sm
     </div>
     <div>
         <label>PRECIO</label>
-        <input type="text" name="PRECIO">
+        <input type="value" name="PRECIO">
     </div>
     <button class="btn" type="submit">Editar Producto</button>
 </form>
